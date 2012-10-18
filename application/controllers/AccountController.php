759791deae0c15f9debe5dbc9212443bcd5821be
AaronJiang <jianglong14@gmail.com>
2012-10-18 16:02:52 +0800

    Aaron, firsh push

---
+++ application/controllers/AccountController.php
@@ -0,0 +1,50 @@
+<?php
+
+class AccountController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    /**
+     * Process the account form
+     */
+    public function successAction()
+    {
+        // action body
+    }
+
+    /**
+     * Display the form for signing up
+     */
+    public function newAction()
+    {
+        // action body
+    }
+
+    /**
+     * Activate Account. Used once the user
+     * receuves a welcome email and decides to authenticate
+     * their account
+     */
+    public function activateAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
---
+++ application/controllers/ArtistController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Artist Controller.
+ */
+class ArtistController extends Zend_Controller_Action
+{
+	/**
+	 * List all the artists in the system.
+	 *
+	 */
+	public function listAllArtistsAction()
+	{
+		
+	}
+
+}
